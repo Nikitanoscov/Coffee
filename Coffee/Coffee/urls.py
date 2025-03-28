95e4bc0f3207cf5ad2ddb5b19594a041cea140d5
Nikita Noskov <nik030205@mail.ru>
2025-03-28 23:32:11 +0300

    Add urls from mvt app

--- Coffee/Coffee/urls.py
+++ Coffee/Coffee/urls.py
@@ -1,15 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+    path('', include('mvt.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
