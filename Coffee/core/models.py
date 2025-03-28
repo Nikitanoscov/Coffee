451faced91445e39e73111913fcabdc7d9ea2242
Nikita Noskov <nik030205@mail.ru>
2025-03-28 23:32:46 +0300

    Create Order and OrderItem models and create migrations

--- Coffee/core/models.py
+++ Coffee/core/models.py
@@ -8,35 +8,38 @@ class Orders(models.Model):
         ('paid', 'Оплачено'),
     ]
 
-
     table_number = models.PositiveSmallIntegerField(
         verbose_name='Номер стола'
     )
-    status = models.Choices(
-        STATUS_CHOICES,
+    status = models.CharField(
+        choices=STATUS_CHOICES,
+        max_length=20
     )
-    
-    def calculate_total_price(self):
-        pass
 
     @property
-    def total_sum(self):
-        return self.calculate_total_price()
-    
+    def total_price(self):
+        """
+        Динамическое вычисление общей стоимости заказа.
+        Учитываются цены и количество всех блюд, связанных с заказом.
+        """
+        return sum(item.price * item.quantity for item in self.items.all())
 
 
 class OrdersItems(models.Model):
 
     order = models.ForeignKey(
         Orders,
-        related_name='items'
+        related_name='items',
+        on_delete=models.CASCADE
     )
     dish = models.CharField(
         max_length=50,
         verbose_name='Блюдо',
     )
-    price = models.PositiveBigIntegerField(
-        verbose_name='Цена',
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Цена'
     )
     quantity = models.PositiveSmallIntegerField(
         verbose_name='Количество',
