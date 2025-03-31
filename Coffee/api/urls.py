9ff981d06acb2a34690f160ae9a42b665be64046
Nikita Noskov <nik030205@mail.ru>
2025-04-01 02:39:13 +0300

    Create Urls for api

--- Coffee/api/urls.py
+++ Coffee/api/urls.py
@@ -1,8 +1,15 @@
+from django.urls import include, path
 from rest_framework import routers
 
+from .views import OrdersViewSet, RevenueView
+
 router = routers.DefaultRouter()
-router.register('orders', )
+router.register(r'orders', OrdersViewSet, basename='order')
 
 urlpatterns = [
-    
+    path(
+        'orders/revenue/',
+        RevenueView.as_view()
+    ),
+    path('', include(router.urls)),
 ]
