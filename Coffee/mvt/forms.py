2e48238e06b6edac6038bd2af651147d8a91d863
Nikita Noskov <nik030205@mail.ru>
2025-03-31 21:39:03 +0300

    Update forms: Dividing the form into creation and editing

--- Coffee/mvt/forms.py
+++ Coffee/mvt/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import inlineformset_factory
+from django.forms import inlineformset_factory, ValidationError
 
 from core.models import Orders, OrdersItems
 
@@ -8,8 +8,15 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Orders
         fields = (
+            'status',
+        )
+
+
+class CreateOrderForm(forms.ModelForm):
+    class Meta:
+        model = Orders
+        fields = (
             'table_number',
-            'status'
         )
 
 
@@ -23,10 +30,26 @@ class OrderItemForm(forms.ModelForm):
         )
 
 
-OrderItemFormSet = inlineformset_factory(
+class ValidateFormSet(forms.BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        has_valid_item = False
+        for form in self.forms:
+            if form.cleaned_data and not form.cleaned_data.get('DELETE'):
+                dish = form.cleaned_data.get('dish')
+                price = form.cleaned_data.get('price')
+                quantity = form.cleaned_data.get('quantity')
+                if dish and price and quantity:
+                    has_valid_item = True
+        if not has_valid_item:
+            raise ValidationError('Необходимо добавить хотя бы одно блюдо с заполненными всеми полями.')
+
+
+OrdersItemsFormSet = inlineformset_factory(
     Orders,
     OrdersItems,
     form=OrderItemForm,
+    formset=ValidateFormSet,
     extra=1,
     can_delete=True
 )
