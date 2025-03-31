ff202eb48aea887d7f369779156387b7ce4f3bd3
Nikita Noskov <nik030205@mail.ru>
2025-04-01 02:40:28 +0300

    Create viewset for work to orders and view for revenue

--- Coffee/api/views.py
+++ Coffee/api/views.py
@@ -1,7 +1,40 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import SAFE_METHODS
+from django.db.models import Sum
 
 from core.models import Orders
+from core.serializers import UpdateOrdersSerializer, CreateOrdersSerializer
 
 
-class Orders(ModelViewSet):
+class OrdersViewSet(ModelViewSet):
     queryset = Orders.objects.prefetch_related('items')
+    serializer_class = CreateOrdersSerializer
+    filterset_fields = ['id', 'status']
+
+    def get_serializer_class(self):
+        if self.request.method in ('PUT', 'PATCH'):
+            return UpdateOrdersSerializer
+        return super().get_serializer_class()
+
+
+class RevenueView(ListAPIView):
+    queryset = Orders.objects.prefetch_related(
+        'items'
+    ).filter(
+        status='Оплачено'
+    )
+    serializer_class = CreateOrdersSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        total_revenue = sum(order.total_price for order in queryset)
+        serializer_data = self.get_serializer(
+            queryset,
+            many=True
+        ).data
+        return Response({
+            'results': serializer_data,
+            'total_revenue': total_revenue
+        })
